48cae2a77c4d69ba6e675a4f757c16dff6860a14
abe <user1test2024@outlook.jp>
2024-03-24 13:11:22 +0900

    更新分のコミットです。

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+	
+	@GetMapping("/get")
+	public String sample() {
+		return "はじめてのSpring";
+	}
+
+}
+
